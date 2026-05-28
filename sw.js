f11f7e3aaa47315fd2daf4d7c2af71dacb82351d
bettyapanit <bettyapanit@gmail.com>
2026-05-28 17:55:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-const CACHE = 'reserve-v4';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/jungle.jpg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
